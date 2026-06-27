1b3def2611add1f94680d494a514aae79fc5901d
Victoria <hannvi02gem@gmail.com>
2026-06-26 19:59:55 -0600

    primer commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("¡EL TOKEN DE TU BOT DEBERÍA ESTAR AQUÍ!")
